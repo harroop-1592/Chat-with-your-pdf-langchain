a4eceec493cbc00cb0955a4452d9e40d08a224bd
Harroop Singh <harroop.1592@gmail.com>
2023-10-19 13:35:21 -0700

    Adding requirements.txt and app.py files - initial running code

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+# Sidebar contents
+with st.sidebar:
+    st.title('LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [openAi](https://platform.openai.com/docs/models) LLM model
+                
+''')
+add_vertical_space(5)
+st.write('Made by [Prompt Engineer](https://youtube.com/@engineerprompt)')
