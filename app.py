cc9dc7a3e61c8801c35ef6b497e3195989b70700
Harroop Singh <harroop.1592@gmail.com>
2023-10-19 20:50:48 -0700

    Modified app.py file to add pdf loader code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 # Sidebar contents
@@ -11,6 +12,26 @@ with st.sidebar:
     - [LangChain](https://python.langchain.com/)
     - [openAi](https://platform.openai.com/docs/models) LLM model
                 
-''')
-add_vertical_space(5)
-st.write('Made by [Prompt Engineer](https://youtube.com/@engineerprompt)')
+    ''')
+    add_vertical_space(5)
+    st.write('Made by [Prompt Engineer](https://youtube.com/@engineerprompt)')
+
+def main():
+    st.header("Chat with your PDF ")
+
+    # Upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+
+    st.write(pdf)
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+            st.write(text)
+
+if __name__ == '__main__':
+    main()
